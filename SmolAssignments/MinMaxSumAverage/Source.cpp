b98d283e241c38cae9225db330547b323642e0e5
lokr934 <emilia.lokrantz@futuregames.nu>
2022-02-21 08:45:24 +0100

    Started minMasSumAvrg

---
+++ SmolAssignments/MinMaxSumAverage/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void MinMaxSumAverage(int& arr)
+{
+	int min;
+	int max;
+	int sum;
+	int average;
+
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+
+}

b694fd11d1e35e2ff8923b2ce91eba80b19064db
lokr934 <emilia.lokrantz@futuregames.nu>
2022-02-22 15:20:11 +0100

    minmaxsumaverage finished

--- SmolAssignments/MinMaxSumAverage/Source.cpp
+++ SmolAssignments/MinMaxSumAverage/Source.cpp
@@ -2,17 +2,36 @@
 
 using namespace std;
 
-void MinMaxSumAverage(int& arr)
+void MinMaxSumAverage(int* arr, int length)
 {
-	int min;
-	int max;
-	int sum;
-	int average;
+	
+	int min = arr[0];
+	int max = arr[0];
+	int sum{0};
+	for (int i = 0; i < length; i++)
+	{
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
 
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+		sum += arr[i];
+	}
+	const int average = sum / length;
+
+	cout << "Smallest value: " << min << endl;
+	cout << "Largest value: " << max << endl;
+	cout << "Total sum: " << sum << endl;
+	cout << "Average: " << average << endl;
 }
 
 int main()
 {
 	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-
+	int length = size(arr);
+	MinMaxSumAverage(arr, length);
 }
